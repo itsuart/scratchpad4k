c8456098bebc760fddb58b54de77a77a6eacd994
itsuart <no@email.privacy>
2026-08-08 11:25:43 +0700

    add ignore inserts when generating error string

--- src/helpers/error_message.cpp
+++ src/helpers/error_message.cpp
@@ -4,7 +4,7 @@ namespace helpers {
     std::unique_ptr<char, decltype(LocalFree)*> get_error_message_a(DWORD errorCode) noexcept {
         char* message = nullptr;
         ::FormatMessageA(
-            FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+            FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
             nullptr, errorCode, 0, (char*)&message, 1, nullptr
         );
 
@@ -14,7 +14,7 @@ namespace helpers {
     std::unique_ptr<wchar_t, decltype(LocalFree)*> get_error_message_w(DWORD errorCode) noexcept {
         wchar_t* message = nullptr;
         ::FormatMessageW(
-            FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+            FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
             nullptr, errorCode, 0, (wchar_t*)&message, 1, nullptr
         );
 
